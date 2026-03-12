30bf8e7c5179b75d3438cf19208772bb9565da80
atharva bhosale <atharvabhosale@atharvas-MacBook-Air.local>
2026-03-13 02:34:59 +0530

    fix mongodb connection

--- restaurant-evaluation/backend/config/db.js
+++ restaurant-evaluation/backend/config/db.js
@@ -2,15 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    });
-
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
   } catch (error) {
-    console.error("MongoDB connection failed:");
-    console.error(error.message);
+    console.error("MongoDB connection failed:", error.message);
     process.exit(1);
   }
 };
